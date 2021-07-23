1ee3c0781d8d5b267276a8ca9447f48f65d7c4ed
Robert Saravia <robertocarlossaraviadiaz@gmail.com>
2021-07-23 03:20:39 -0400

    Update Launcher.jsx

--- src/components/Launcher.jsx
+++ src/components/Launcher.jsx
@@ -19,9 +19,9 @@ export default class Launcher extends React.Component {
     componentDidMount() {
         SMART.authorize({
             clientId: "127afa4e-286f-47ae-ac19-ecc3d7f4ddac",
-            scope: "launch launch/patient patient/read online_access openid profile",
+            scope: "launch patient/Patient.read online_access openid profile",
             redirectUri: "./Home",
-            iss: "https://launch.smarthealthit.org/v/r4/sim/eyJrIjoiMSJ9/fhir"
+            iss: "https://launch.smarthealthit.org/v/r4/sim/eyJrIjoiMSJ9/fhir",
 
             // WARNING: completeInTarget=true is needed to make this work
             // in the codesandbox frame. It is otherwise not needed if the
