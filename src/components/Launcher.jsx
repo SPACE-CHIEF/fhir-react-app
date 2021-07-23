f0cf6018f820a9b67e93788a7def24f41f4c913b
Robert Saravia <robertocarlossaraviadiaz@gmail.com>
2021-07-23 04:08:04 -0400

    Update Launcher.jsx

--- src/components/Launcher.jsx
+++ src/components/Launcher.jsx
@@ -21,6 +21,7 @@ export default class Launcher extends React.Component {
             clientId: "127afa4e-286f-47ae-ac19-ecc3d7f4ddac",
             scope: "launch patient/Patient.read online_access openid profile",
             redirectUri: "./Home",
+            // iss: "https://launch.smarthealthit.org/v/r4/sim/eyJrIjoiMSIsImIiOiI4N2EzMzlkMC04Y2FlLTQxOGUtODljNy04NjUxZTZhYWIzYzYifQ/fhir",
             iss: "https://launch.smarthealthit.org/v/r4/sim/eyJrIjoiMSJ9/fhir",
 
             // WARNING: completeInTarget=true is needed to make this work
