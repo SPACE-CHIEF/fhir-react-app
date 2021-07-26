2ee9e2398b858974b56fddf36507d01b86af2337
Robert Saravia <robertocarlossaraviadiaz@gmail.com>
2021-07-26 13:42:31 -0400

    Update Launcher.jsx

--- src/components/Launcher.jsx
+++ src/components/Launcher.jsx
@@ -19,10 +19,10 @@ export default class Launcher extends React.Component {
     componentDidMount() {
         SMART.authorize({
             clientId: "127afa4e-286f-47ae-ac19-ecc3d7f4ddac",
-            scope: "launch profile fhirUser openid online_access launch/patient",
+            scope: "launch patient/Patient.read online_access openid profile",
             redirectUri: "./Home",
+            // iss: "https://launch.smarthealthit.org/v/r4/sim/eyJrIjoiMSIsImIiOiI4N2EzMzlkMC04Y2FlLTQxOGUtODljNy04NjUxZTZhYWIzYzYifQ/fhir",
             iss: "https://launch.smarthealthit.org/v/r4/sim/eyJrIjoiMSJ9/fhir",
-            // iss: "https://fhir-open.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d/Patient?_12724067" // API for cerner.
             // try "./" to test with cerner sandbox
             // WARNING: completeInTarget=true is needed to make this work
             // in the codesandbox frame. It is otherwise not needed if the
