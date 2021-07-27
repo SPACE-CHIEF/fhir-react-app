59f1d1e1714bd0c55a524ac706d5254bebe937d3
Robert Saravia <robertocarlossaraviadiaz@gmail.com>
2021-07-26 20:15:50 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { Navigation, Home }  from "./components"
 import { Appointments, MyGenetics, Launcher } from "./components";
 import './scss/style.scss'
@@ -7,7 +7,7 @@ import './scss/style.scss'
 export default function App() {
   return (
       //<a id="top"></a>
-        <Router>
+      <div className="App">
             <Navigation/>
             <Switch>
               <Route exact path="/" exact component={() => <Launcher/>}/>
@@ -17,6 +17,6 @@ export default function App() {
             </Switch>
             {/* <MobileMenu/> */}
             {/* <Footer/> */}
-        </Router>
+      </div>
   );
 }
