ad6b39c127c9de7995fed20b8d699ea6c17c574d
Robert Saravia <robertocarlossaraviadiaz@gmail.com>
2021-07-26 23:19:56 -0400

    launch

--- src/components/Launcher.jsx
+++ src/components/Launcher.jsx
@@ -36,12 +36,12 @@ export default class Launcher extends React.Component {
     componentDidMount() {
         SMART.authorize({
             clientId: "127afa4e-286f-47ae-ac19-ecc3d7f4ddac",
-            scope: "launch patient/Patient.read online_access openid profile",
+            scope: "launch patient/Patient.read offline_access openid profile",
             redirectUri: "https://fhir-react.herokuapp.com/Home",
             // iss: "https://launch.smarthealthit.org/v/r4/sim/eyJrIjoiMSIsImIiOiI4N2EzMzlkMC04Y2FlLTQxOGUtODljNy04NjUxZTZhYWIzYzYifQ/fhir"
             iss: "https://fhir-open.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d/",
             // iss: "https://launch.smarthealthit.org/v/r4/sim/eyJrIjoiMSJ9/fhir/",
-            launch: "https://fhir-react.herokuapp.com"
+            launch: "https://fhir-react.herokuapp.com/"
         });
     }
     /**
